c25f8c9b5ec4c0674d727b5d3f7a6d748d3bd040
jkim1014 <joonyoungkim@college.harvard.edu>
2019-01-10 13:23:19 -0500

    Create table structure for transactions

---
+++ src/db/migrations/20190110131712_merchants.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+exports.up = knex => {
+  return knex.schema.createTable('merchants', function(table) {
+    table
+      .increments('primaryId')
+      .notNullable()
+      .unsigned()
+
+    table.string('name').notNullable()
+
+    table.string('category').notNullable()
+
+    table.string('streetNum').notNullable()
+
+    table.string('streetName').notNullable()
+
+    table.string('city').notNullable()
+
+    table.string('state').notNullable()
+
+    table.string('zip').notNullable()
+
+    table
+      .integer('numTransactions')
+      .notNullable()
+      .unsigned()
+
+    table.float('avgAmount').notNullable()
+
+    table.float('lon')
+    
+    table.float('lat')
+  })
+}
+
+exports.down = async knex => knex.schema.dropTable('merchants')
